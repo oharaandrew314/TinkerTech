212bad7cfd46881aab5e889a4eb0b4290ae16cf3
oharaandrew314 <ohara.andrew314@gmail.com>
2016-05-24 22:13:03 -0400

    add help menu functionality

--- src/main/io/andrewohara/tinkertech/Main.java
+++ src/main/io/andrewohara/tinkertech/Main.java
@@ -1,12 +1,15 @@
 package io.andrewohara.tinkertech;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Key;
 import com.google.inject.name.Names;
 
+import io.andrewohara.tinkertech.models.Version;
 import io.andrewohara.tinkertech.services.DirectoryWatchService;
 import io.andrewohara.tinkertech.views.GuiModule;
 import javafx.application.Application;
@@ -16,7 +19,21 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-	public static final String APP_NAME = "Tinker Tech - Factorio Mod Manager";
+	public static final String
+	APP_NAME = "Tinker Tech - Factorio Mod Manager",
+	AUTHOR = "Andrew O'Hara";
+
+	public static final Version VERSION = Version.valueOf("0.1.0");
+
+	public static final URL HELP_URL, MAIL_URL;
+	static {
+		try {
+			HELP_URL = new URL("https://github.com/oharaandrew314/TinkerTech/wiki");
+			MAIL_URL = new URL("mailto:tinkertech@andrewohara.io");
+		} catch (MalformedURLException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 	private final FXMLLoader mainPaneLoader;
 	private final DirectoryWatchService directoryWatchService;
--- src/main/io/andrewohara/tinkertech/views/MainPaneController.java
+++ src/main/io/andrewohara/tinkertech/views/MainPaneController.java
@@ -1,10 +1,17 @@
 package io.andrewohara.tinkertech.views;
 
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 import com.google.inject.Inject;
 
+import io.andrewohara.tinkertech.Main;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.HBox;
 
 public class MainPaneController {
@@ -24,6 +31,29 @@ public class MainPaneController {
 		try {
 			application.stop();
 		} catch (Exception e) {
+			errorHandler.handleError(e);
+		}
+	}
+
+	@FXML protected void handleAbout(ActionEvent event) {
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("About");
+		alert.setHeaderText(String.format("%s by %s - v%s", Main.APP_NAME, Main.AUTHOR, Main.VERSION));
+		alert.show();
+	}
+
+	@FXML protected void handleContact(ActionEvent event) {
+		try {
+			Desktop.getDesktop().mail(Main.MAIL_URL.toURI());
+		} catch (IOException | URISyntaxException e) {
+			errorHandler.handleError(e);
+		}
+	}
+
+	@FXML protected void handleHelp(ActionEvent event) {
+		try {
+			Desktop.getDesktop().browse(Main.HELP_URL.toURI());
+		} catch (IOException | URISyntaxException e) {
 			errorHandler.handleError(e);
 		}
 	}
